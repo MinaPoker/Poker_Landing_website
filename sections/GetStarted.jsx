8b9d480b11091b848d084f8c4913d549702f233e
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-17 17:22:25 +0530

    Build visually appealing GetStarted section with animated features and image

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,14 +1,16 @@
-'use client';
+'use client'; // Initializing the client
 
+// Importing necessary modules, components, styles, and motion utilities
 import { motion } from 'framer-motion';
-
 import styles from '../styles';
 import { startingFeatures } from '../constants';
 import { StartSteps, TitleText, TypingText } from '../components';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
 
+// Functional component for GetStarted section
 const GetStarted = () => (
   <section className={`${styles.paddings} relative z-10`}>
+    {/* Motion animation for the container */}
     <motion.div
       variants={staggerContainer}
       initial="hidden"
@@ -16,24 +18,31 @@ const GetStarted = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
+      {/* Motion animation for the left section */}
       <motion.div
         variants={planetVariants('left')}
         className={`flex-1 ${styles.flexCenter} rounded-full`}
       >
+        {/* Image component */}
         <img
           src="/Formats.jpeg"
           alt="Formats"
           className="w-[90%] h-[90%] object-contain rounded-2xl"
         />
       </motion.div>
+      {/* Motion animation for the right section */}
       <motion.div
         variants={fadeIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
+        {/* Component for typing effect */}
         <TypingText title="| Formats" />
+        {/* Component for displaying title */}
         <TitleText title={<>What Formats We Offer</>} />
+        {/* Displaying a list of features */}
         <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
           {startingFeatures.map((feature, index) => (
+            // Component to display steps with numbers and text
             <StartSteps
               key={feature}
               number={`${index < 10 ? '0' : ''} ${index + 1}`}
@@ -46,4 +55,4 @@ const GetStarted = () => (
   </section>
 );
 
-export default GetStarted;
+export default GetStarted; // Exporting the GetStarted component
--- sections/Index.js
+++ sections/Index.js
@@ -1,17 +1,31 @@
+// Import the 'Hero' component from the corresponding file
 import Hero from "./Hero";
+
+// Import the 'Explore' component from the corresponding file
 import Explore from "./Explore";
+
+// Import the 'About' component from the corresponding file
 import About from "./About";
+
+// Import the 'GetStarted' component from the corresponding file
 import GetStarted from "./GetStarted";
+
+// Import the 'Feedback' component from the corresponding file
 import Feedback from "./Feedback";
+
+// Import the 'World' component from the corresponding file
 import World from "./World";
+
+// Import the 'Roadmap' component from the corresponding file
 import Roadmap from "./Roadmap";
 
+// Export all the components for use in other parts of the application
 export {
-    Hero,
-    About,
-    Explore,
-    GetStarted,
-    Feedback,
-    World,
-    Roadmap
+    Hero,        // Hero section, often the main banner of a website
+    About,       // About section providing information about the project or team
+    Explore,     // Explore section showcasing project features or offerings
+    GetStarted,  // GetStarted section guiding users on how to get started
+    Feedback,    // Feedback section for user reviews or testimonials
+    World,       // World component, purpose may vary based on the project
+    Roadmap      // Roadmap section outlining the project's development plan
 }
