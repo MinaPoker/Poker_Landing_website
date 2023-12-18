cb3dcf4369361aa305db9301e5b9e0b8a0cbc4a0
VishalPachpor <Vishalpatil080502@gmail.com>
2023-12-19 01:48:35 +0530

    Extract motion variants to a separate utility file for reusability

---
+++ sections/GetStarted.jsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { startingFeatures } from '../constants';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+
+const GetStarted = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={planetVariants('left')}
+        className={`flex-1 ${styles.flexCenter} rounded-full`}
+      >
+        <img
+          src="/Formats.jpeg"
+          alt="Formats"
+          className="w-[90%] h-[90%] object-contain rounded-2xl"
+        />
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="| Formats" />
+        <TitleText title={<>What Formats We Offer</>} />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps
+              key={feature}
+              number={`${index < 10 ? '0' : ''} ${index + 1}`}
+              text={feature}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default GetStarted;
