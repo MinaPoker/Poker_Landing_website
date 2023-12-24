dbba19043def94d03caca6d2fbbb87ef57d33617
VishalPachpor <Vishalpatil080502@gmail.com>
2023-12-24 20:07:40 +0530

    Implement World component showcasing global player connectivit

--- sections/Index.js
+++ sections/Index.js
@@ -3,10 +3,13 @@ import Explore from "./Explore";
 import About from "./About";
 import GetStarted from "./GetStarted";
 import Feedback from "./Feedback";
+import World from "./World";
 
 export {
     Hero,
     About,
     Explore,
-    GetStarted
+    GetStarted,
+    Feedback,
+    World,
 }
