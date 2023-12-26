916f82c58799264dc5ad03cafac652a5235c51e6
VishalPachpor <Vishalpatil080502@gmail.com>
2023-12-26 12:18:04 +0530

    Implement StarWrapper Higher-Order Component

--- app/page.js
+++ app/page.js
@@ -7,24 +7,27 @@ import { Roadmap } from '@/sections/Index'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className='bg-primary-black overflow-hidden'>
+    <div className="bg-primary-black overflow-hidden">
     <Navbar />
-    <Hero/>
-    <div className="gradient-03 z-0" />
+    <Hero />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
       <Explore />
     </div>
-
-    <div className='relative'>
-    <GetStarted />
-      <div className='relative'>
-        <Roadmap />
-      </div>
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <Experience />
     </div>
-
-    <div className='relative'>
-    <Insights />
+    <World />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
     </div>
-      <About />
+    <Footer />
+  </div>
     </main>
   )
 }
